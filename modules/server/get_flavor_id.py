0b0c8b13e6023a6de686e55186ecd9ddb53b8401
unknown <mhicks071@yahoo.com>
2021-01-15 09:44:38 -0500

    Exercises for server module that won't have a dependency on nonfunctional functions

---
+++ modules/server/get_flavor_id.py
@@ -0,0 +1,19 @@
+import chi
+from chi.server import *
+
+BLAZAR_TIME_FORMAT = '%Y-%m-%d %H:%M'
+
+# Before doing anything, we must set some constants to be used in this exercise.
+
+# Configure project and site...
+chi.set('project_name', 'CH-822154') # Set to your project name
+chi.set('region_name', 'CHI@UC') # Optional - defaults to CHI@UC
+
+# Define keypair name...
+key_name = 'my_chameleon_key' # Change to your keypair name
+
+# Set constants...
+default_flavor = 'baremetal'
+
+# Get a flavor by name.
+get_flavor_id(default_flavor)
---
+++ modules/server/show_flavor.py
@@ -0,0 +1,22 @@
+import chi
+from chi.server import *
+
+BLAZAR_TIME_FORMAT = '%Y-%m-%d %H:%M'
+
+# Before doing anything, we must set some constants to be used in this exercise.
+
+# Configure project and site...
+chi.set('project_name', 'CH-822154') # Set to your project name
+chi.set('region_name', 'CHI@UC') # Optional - defaults to CHI@UC
+
+# Define keypair name...
+key_name = 'my_chameleon_key' # Change to your keypair name
+
+# Set constants...
+default_flavor = 'baremetal'
+
+# Get a flavor by name.
+flavor_id = get_flavor_id(default_flavor)
+
+# Get a flavor by its ID.
+flavor = show_flavor(flavor_id)
